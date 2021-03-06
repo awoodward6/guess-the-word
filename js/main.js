3cc18877bde75452f28c815252f29453822a5964
Allyn Woodward <allyn.wd@outlook.com>
2021-03-05 21:20:15 -0500

    Second commit with base HTML, base css, constant and variable outline

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 /*----- constants -----*/
-
-
+const questions = {
+    one: {
+        question: "Question 1",
+        answer: "TLC",
+    },
+    two: {
+        question: "Question 2",
+        answer: "Spice Girls",
+    },
+    three: {
+        question: "Question 3",
+        answer: "Clueless",
+    },
+    four: {
+        question:"Based off the Taming of the Shrew, this 1999 movie, ...",
+        answer: "10 Things I Hate About You",
+    }
+    five: {
+        question: "Question 5",
+        answer: "Answer 5",
+    }
+};
 
 /*----- app's state (variables) -----*/
-
+let turn, round, finish;
 
 
 /*----- cached element references -----*/
-
+document.querySelector('click', )
 
 
 /*----- event listeners -----*/
