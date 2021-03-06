8e58085ec085c53e8635a152a9c540cf0637c0c7
Allyn Woodward <allyn.wd@outlook.com>
2021-03-06 16:17:14 -0500

    Console log constant questions for q&a section of object

--- js/main.js
+++ js/main.js
@@ -2,33 +2,33 @@
 const questions = {
     one: {
         question: "Question 1",
-        answer: "TLC",
+        answer: "Answer 1",
     },
     two: {
         question: "Question 2",
-        answer: "Spice Girls",
+        answer: "Answer 2",
     },
     three: {
         question: "Question 3",
-        answer: "Clueless",
+        answer: "Answer 3",
     },
     four: {
         question:"Question 4",
-        answer: "10 Things I Hate About You",
-    }
-    five: {
-        question: "Question 5",
-        answer: "Answer 5",
+        answer: "Answer 4",
     }
 };
 
+console.log(questions.one.answer);
+
 /*----- app's state (variables) -----*/
-let turn, round, finish;
+// let turn, round, finish;
 
 
 /*----- cached element references -----*/
-document.querySelector('click', )
-
+// document.querySelector('click', ) //event click on keyboard, correct answer
+// document.querySelector('click', ) //event click on keyboard, wrong answer
+// document.querySelector('click', replay); //event click on replay button
+// console.log(replay);
 
 /*----- event listeners -----*/
 
