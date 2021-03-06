a63c11994b93b604fac6784eba9b04ada8616a1e
Allyn Woodward <allyn.wd@outlook.com>
2021-03-06 17:06:46 -0500

    Updating state variable and element references

--- js/main.js
+++ js/main.js
@@ -2,44 +2,47 @@
 const questions = {
     one: {
         question: "Question 1",
+        hint: "Hint 1", //should hint go in objects constant or cached element references
         answer: "Answer 1",
     },
     two: {
         question: "Question 2",
+        hint: "Hint 2",
         answer: "Answer 2",
     },
     three: {
         question: "Question 3",
+        hint: "Hint 3"
         answer: "Answer 3",
     },
     four: {
         question:"Question 4",
+        hint: "Hint 4"
         answer: "Answer 4",
     }
 };
 
-const smileFace = {
-    circle: "https://i.imgur.com/P43MFUE.png",
-    eyeOne: "https://i.imgur.com/BLTHZ7B.png",
-    eyeTwo: "https://i.imgur.com/scyFxgR.png",
-    smile: "https://i.imgur.com/3JBMoLM.png",
-    fill: "https://i.imgur.com/ycqq97C.png",
-};
 // console.log(questions.one.answer);
-// console.log(smileFace.fill);
+
 
 /*----- app's state (variables) -----*/
-// let turn, round, finish; //variables that change throughout the time of the game/ as we interact w/ game
-
+let turn, answer, round, finish; //variables that change throughout the time of the game/ as we interact w/ game
 
 /*----- cached element references -----*/
-// document.querySelector('click', ) //event click on keyboard, correct answer
-// document.querySelector('click', keyboard) //event click on keyboard, wrong answer
-// document.querySelector('click', replay); //event click on replay button
-// console.log(replay);
+const smileFace = {
+    circle: document.querySelector("circle"),
+    eyeOne: document.querySelector("eye1"),
+    eyeTwo: document.querySelector("eye2"),
+    smile: document.querySelector("smile"),
+    fill: document.querySelector("fill"),
+};
+// console.log(smileFace.fill);
 
 /*----- event listeners -----*/
-
+// document.querySelector('click', ) //event click on keyboard, correct answer
+// document.querySelector('click', keyboard) //event click on keyboard, wrong answer
+document.querySelector('click', replay); //event click on replay button
+console.log(replay);
 
 
 /*----- functions -----*/
