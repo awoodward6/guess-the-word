20e29c97c6ef63c4d7e39e50cad053b480d9a104
Allyn Woodward <allyn.wd@outlook.com>
2021-03-09 12:02:39 -0500

    Commiting for deployment lesson

--- js/main.js
+++ js/main.js
@@ -3,36 +3,24 @@ const questions = {
     one: {
         question: "Question 1",
         hint: "Hint 1", 
+        answer: "Answer 1", 
     },
     two: {
         question: "Question 2",
         hint: "Hint 2",
+        answer: "Answer 2",
     },
     three: {
         question: "Question 3",
         hint: "Hint 3",
+        answer: "Answer 3",
     },
     four: {
         question:"Question 4",
         hint: "Hint 4",
-    },
-};
-// console.log(questions.one.answer);
-
-const answers = {
-    one: {
-        answer: "Answer 1", 
-    },
-    two: {
-        answer: "Answer 2",
-    },
-    three: {
-        answer: "Answer 3",
-    },
-    four: {
         answer: "Answer 4",
     },
-}; 
+};
 
 const smileFace = {
     circle: document.querySelector("#circle"),
@@ -42,16 +30,19 @@ const smileFace = {
     fill: document.querySelector("#fill"),
     'null': null, //null when no guess. 
 };
-//  console.log(smileFace.fill);
 
-/*----- app's state (variables) -----*/ //variables that change as we interact w/ game.
-let key, question, guess, finish; 
+const keyStatus = {
+    'null': null,
+    '1': "green",
+    '-1': "red",
+};
 
-/*----- cached element references -----*/ //elements update appearance of using javascript
+/*----- app's state (variables) -----*/ 
+let key, question, guess, end; 
+
+/*----- cached element references -----*/ 
 const q= document.querySelector("#question");
-const h=document.querySelector("#hint");
 const message= document.querySelector("#message");
-// console.log("message");
 
 /*----- event listeners -----*/ 
 document.querySelector("#keyboard").addEventListener('click', keyboard); 
@@ -59,32 +50,61 @@ document.querySelector("#replay").addEventListener("click", replay);
 
 /*----- functions -----*/
 function init() {
-    key = new Array(26); //key var is the board
-    let questionArray= Object.entries(questions)
+    key = new Array(26); 
+
+    let questionArray= Object.entries(questions);
     let idx = Math.floor(Math.random() * questionArray.length);
     question = questionArray[idx][1];
-    console.log(question);
-    guess = 1; //guess is the user individual selection
-    finish = null; //finish var is final q - break - end game. start over with replay button visible. 
+    // console.log(question);
+
+    guess = 1; 
+    end = null; 
     render()
 };
 init();
 
-function render() {  //taking state variables and putting it in the DOM/screen
-    document.querySelector("#question").innerHTML = `${question.question} ${question.hint}`;
-    // replay.style.display = "none";
-    // if (guess === "complete") {
-    //   message.textContent= "It's cashed. Next round!";
-    //   replay.style.display = "block";
-    // } else if {
-    //     return;
-    // };
+function render() {  
+    key.forEach(function(btn, idx) {
+        document.getElementById(idx).style.color= keyStatus[btn];
+    });
+    document.querySelector("#replay").style.visibility = "hidden";
+    document.querySelector("#hangman").style.visibility = "hidden";
+    q.innerHTML = `${question.question} ${question.hint}`;
+
+    document.querySelector("#answer").innerHTML = `${question.answer}`;
+    document.querySelector("#answer").style.visibility = "hidden";
+
+    if (end === "complete") {
+        message.textContent= "Play again!";
+        document.querySelector("#replay").style.visibility  = "visible";
+    } else {
+         return;
+    };
 };
 
-function keyboard() {
-    // console.log(evt.target);
+function keyboard(evt) {
+    
+    // let key = document.querySelector("#keyboard");
+    //     key.addEventListener("click", function(evt) {
+    //     if key === key[idx]
+    // });
+    // // render();
+    // console.log(evt);
 };
 
-function replay() {
-//    console.log("We made it");
-};
+// function checkGuess() {
+//     for (let ans in question) {
+//         console.log(ans);
+//     };
+// };
+
+// function end() {
+//     key.forEach(function(scenario) {
+//         if (key[scenario[idx]] < 5) {
+//             end = guess;
+//         };
+// });
+
+// function replay() {
+// //    console.log("We made it");
+// };
